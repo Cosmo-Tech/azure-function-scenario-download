46fbcdf24fb0494d8118c3b6cca10755da105c21
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-03-09 11:14:40 +0100

    Moved generate_main and ScenarioDownloader to CoAL

--- ScenarioDownload/__init__.py
+++ ScenarioDownload/__init__.py
@@ -1,12 +1,16 @@
 # Copyright (c) Cosmo Tech corporation.
 # Licensed under the MIT license.
-from cosmotech_scenario_download.generate_main import generate_main
+from CosmoTech_Acceleration_Library.Accelerators.scenario_download.azure_function_main import generate_main
 
 
 def apply_update(content: dict, scenario_data: dict) -> dict:
-    updated_content = content
-    # Apply any transformation on the content here
-    return updated_content
+    """
+    Apply update is the function you will have to implement to specify the logic of your function
+    :param content: Contains all datasets and parameters downloaded from the API
+    :param scenario_data: The data associated to your scenario downloaded from the API
+    :return: A dict of data that should be json equivalent that will be sent as the body of the response of your function call
+    """
+    return content
 
 
 main = generate_main(apply_update=apply_update)
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,9 @@
 # CosmoTech/azure-function-scenario-download
 
-The Azure Function Scenario Download is an accelerator used to allow fast development of web content using the CosmoTech API.
+This function display a usage of the Accelerator created to generate Azure Functions in a Cosmotech API scenario context
+
+## Endpoints
+
+### ScenarioDownload
+
+This simple endpoint is the minimal code required to create an Azure Function, its outputs are documented in the [following page](function_output.md)
